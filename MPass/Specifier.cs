2351ea0ed9cc807b18a6faab79588216baa9635b
Maxwell French <maxwell@frenchnet.org>
2024-03-21 21:41:52 -0400

    Tweak the specifier characters to better align with the Rust version.

--- MPass/Specifier.cs
+++ MPass/Specifier.cs
@@ -19,17 +19,17 @@ public class Specifier
     /// Specifier string syntax is simple.
     /// One or more of the following keys:
     ///     w - lowercase word (examnple)
-    ///     u - uppercase word (EXAMPLE)
+    ///     W - uppercase word (EXAMPLE)
     ///     i - initial caps word (Example)
     ///     r - random cap word (exAmple)
     ///     x - random single alphanumeric character
     ///     z - random single alphanumeric or symbol character (combines x and $)
-    ///     0 - digit, 0-9
+    ///     # - digit, 0-9
     ///     $ - symbol
     ///     (space) - space character
     /// Examples:
-    ///     "i w w 000$" => "Medium test phrase 123!"
-    ///     "ii00$" => "TestPhrase11#"
+    ///     "i w w ###$" => "Medium test phrase 123!"
+    ///     "ii##$" => "TestPhrase11#"
     ///     "zzzzzz" => "t7Eq#r"
     /// </summary>
     /// <exception cref="ArgumentException">Throws if the argument is empty or contains invalid characters.</exception>
@@ -50,7 +50,7 @@ public class Specifier
             ITokenSource tok = c switch
             {
                 'w' => new TokenWord(_words, w => w.ToLower()),
-                'u' => new TokenWord(_words, w => w.ToUpper()),
+                'W' => new TokenWord(_words, w => w.ToUpper()),
                 'i' => new TokenWord(_words, w => w[..1].ToUpper() + w[1..].ToLower()),
                 'r' => new TokenWord(_words, w =>
                 {
@@ -62,7 +62,7 @@ public class Specifier
                 'x' => new TokenChar(new string[] { TokenChar.Numerals, TokenChar.LowerAsciiLetters, TokenChar.UpperAsciiLetters }),
                 'z' => new TokenChar(new string[] { TokenChar.Numerals, TokenChar.LowerAsciiLetters, TokenChar.UpperAsciiLetters, _symbols.Charset }),
                 '$' => _symbols,
-                '0' => new TokenNumber(1),
+                '#' => new TokenNumber(1),
                 ' ' => new TokenSpace(),
                 _ => throw new ArgumentException("Invalid character in specification.")
             };
